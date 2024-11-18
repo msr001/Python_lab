fd3d61883c642e03d1d3099804ac8ebd91087841
Nitesh Kumar <github.lab001@gmail.com>
2024-11-18 23:42:18 +0530

    Update _09_add_days.py.py

--- _09_add_days.py.py
+++ _09_add_days.py.py
@@ -1,16 +1,7 @@
-import os
+import datetime 
+today=datetime.datetime.now().date()
+print("Current date:",today)
 
-# Initialize a counter to keep track of the index
-counter = 1
+day=int(input("Enter days to add :\n"))
 
-# Get the list of all files in the current directory
-for filename in os.listdir():
-    if filename.endswith(".txt"):
-        # Create the new filename with the index and .py extension
-        new_filename = filename.replace(".txt", f"_{counter:02d}.py")
-        os.rename(filename, new_filename)
-        
-        # Increment the counter for the next file
-        counter += 1
-
-print("Files renamed successfully!")
+print("Days after adding: ",today + datetime.timedelta(day))
