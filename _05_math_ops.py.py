77a2d9cc06de25a5eeeb99dc5eef77546cc2db6c
Nitesh Kumar <github.lab001@gmail.com>
2024-11-18 23:34:27 +0530

    Update _05_math_ops.py.py

--- _05_math_ops.py.py
+++ _05_math_ops.py.py
@@ -1,34 +1,36 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-    return a-b
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
 
-def mul(a,b):
-    return a*b
-
-def div(a,b):
-    return a/b
+def div(a, b):
+    if b == 0:
+        return "Division by zero is not allowed."
+    return a / b
 
 while True:
-    print("Arithmetic Operations 😊
-    print("1 to add.\n2 to sub.\n3 to mul.\n4 to div.\n")
-    n=int(input("Enter number :\n"))
-    n1=int(input("Enter num1 :"))
-    n2=int(input("Enter num2 :"))
+    print("Arithmetic Operations")
+    print("1 to add.\n2 to sub.\n3 to mul.\n4 to div.\n5 to exit.")
+    n = int(input("Enter number:\n"))
 
-if n==1:
-    print("Add of entered numbers is :",add(n1,n2))
+    if n == 5:
+        print("Exiting the program.")
+        break
 
-elif n==2:
-    print("Minus of entered numbers is :",sub(n1,n2))
+    n1 = int(input("Enter num1: "))
+    n2 = int(input("Enter num2: "))
 
-elif n==3:
-    print("Multiply of entered numbers is :",mul(n1,n2))
-
-elif n==4:
-    print("Divide of entered numbers is :",div(n1,n2))
-
-else:
-    print("Wrong number. ENter between 1 to 4 only")
+    if n == 1:
+        print("Sum of entered numbers is:", add(n1, n2))
+    elif n == 2:
+        print("Difference of entered numbers is:", sub(n1, n2))
+    elif n == 3:
+        print("Product of entered numbers is:", mul(n1, n2))
+    elif n == 4:
+        print("Division of entered numbers is:", div(n1, n2))
+    else:
+        print("Wrong number. Enter between 1 to 4 only.")
